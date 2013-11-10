185d884fccf3573bf73155d65eec291bcbd03e6f
Anirudha Choudhari <anirudha.choudhari@falkonry.com>
2013-11-10 19:31:44 +0530

    Sample code update...

--- HelloEclipse/src/com/wonderbiz/app/helloeclipse/HelloEclipse.java
+++ HelloEclipse/src/com/wonderbiz/app/helloeclipse/HelloEclipse.java
@@ -12,10 +12,25 @@ public class HelloEclipse {
 		System.out.println("Git is fun...!");
 		System.out.println("Eclipse is cool...!!");
 		
-		Maths mymaths = new Maths(7, 2);
+		Maths mymaths1 = new Maths(7, 2);
 		
-		System.out.println("Maths: ADD = " + mymaths.add());
-		System.out.println("Maths: SUB = " + mymaths.subtract());
+		doMaths(mymaths1);
+		
+		mymaths1.set_num1(89);
+		doMaths(mymaths1);
+		
+		Maths mymaths2 = new Maths(57, 209);
+		
+		doMaths(mymaths2);
+
+		mymaths2.set_num2(635);
+		doMaths(mymaths2);
+	}
+	
+	private static void doMaths(Maths mathsObj) {
+		
+		System.out.println("Maths: ADD [ " + mathsObj.get_num1() + " + " + mathsObj.get_num2() + " ] = " + mathsObj.add());
+		System.out.println("Maths: SUB [ " + mathsObj.get_num1() + " - " + mathsObj.get_num2() + " ] = " + mathsObj.subtract());
 	}
 
 }
--- HelloEclipse/src/com/wonderbiz/lib/maths/Maths.java
+++ HelloEclipse/src/com/wonderbiz/lib/maths/Maths.java
@@ -11,6 +11,22 @@ public class Maths {
 		_num2 = b;
 	}
 	
+	public int get_num1() {
+		return _num1;
+	}
+
+	public void set_num1(int _num1) {
+		this._num1 = _num1;
+	}
+
+	public int get_num2() {
+		return _num2;
+	}
+
+	public void set_num2(int _num2) {
+		this._num2 = _num2;
+	}
+
 	public int add()
 	{
 		return (_num1 + _num2);
